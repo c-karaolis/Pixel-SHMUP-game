e05779a3b8918c526ed79ef9ebcc12409b3045ee
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-07-26 23:25:18 +0300

    refactor of movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,48 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 screenBounds;
-    private Vector2 mousePos;
+    private Vector2 mousePosition;
     private Camera mainCamera;
 
-    private float playerWidth;
-    private float playerHeight;
-    public float boundsOffsetWidth;
-    public float boundsOffsetHeight;
+    private float playerSpriteWidth;
+    private float playerSpriteHeight;
 
-    public float inputSmoothness = 0.01f;
+    [Range(0f,15f)]
+    public float boundsWidthOffset;
+    [Range(0f,15f)]
+    public float boundsHeightOffset;
+    [Range(0f,2f)]
+    public float traversalSmoothness = 0.01f;
 
     void Start()
     {
-        playerWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
-        playerHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+        playerSpriteWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        playerSpriteHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
 
         mainCamera = Camera.main;
 
         screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
 
-        mousePos = Vector2.zero;
+        mousePosition = Vector2.zero;
     }
 
     void Update()
     {
-        if(!Input.GetButton("Fire1")) { return; }
+        if (!Input.GetButton("Fire1")) { return; }
 
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        mousePos = new Vector2(
-            Mathf.Clamp(mousePos.x, screenBounds.x * -1 + playerWidth - boundsOffsetWidth, screenBounds.x - playerWidth + boundsOffsetWidth),
-            Mathf.Clamp(mousePos.y, screenBounds.y * -1 + playerHeight - boundsOffsetHeight, screenBounds.y - playerHeight + boundsOffsetHeight)
+        mousePosition = new Vector2(
+            Mathf.Clamp(mousePosition.x, screenBounds.x * -1 + playerSpriteWidth - boundsWidthOffset, screenBounds.x - playerSpriteWidth + boundsWidthOffset),
+            Mathf.Clamp(mousePosition.y, screenBounds.y * -1 + playerSpriteHeight - boundsHeightOffset, screenBounds.y - playerSpriteHeight + boundsHeightOffset)
             );
 
         Vector3 viewPos = transform.position;
 
         transform.position = new Vector3(
-            Mathf.Lerp(viewPos.x, mousePos.x , inputSmoothness),
-            Mathf.Lerp(viewPos.y, mousePos.y , inputSmoothness),
+            Mathf.Lerp(viewPos.x, mousePosition.x, traversalSmoothness),
+            Mathf.Lerp(viewPos.y, mousePosition.y, traversalSmoothness),
             viewPos.z
             );
     }
