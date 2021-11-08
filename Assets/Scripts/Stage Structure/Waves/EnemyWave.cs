56a7165a0ab33fd17f6c482a257c20af80fbc989
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-11-08 18:10:24 +0200

    restructure

--- Assets/Scripts/Stage Structure/Waves/EnemyWave.cs
+++ Assets/Scripts/Stage Structure/Waves/EnemyWave.cs
@@ -1,16 +1,21 @@
+using Foxlair.Enemies;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyWave : MonoBehaviour
 {
+    public List<EnemySpaceship> enemies;
+    public int NumberOfEnemies { get { return enemies.Count; } }
+
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
     }
 }
